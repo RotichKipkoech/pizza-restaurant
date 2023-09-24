d36cd2811d8b2bc075c5099122afdcb5380f5097
RotichKipkoech <kipkoechrottich@gmail.com>
2023-09-24 21:29:36 +0300

    Define routes and Route to get all restaurants

--- models/route.py
+++ models/route.py
@@ -3,3 +3,11 @@ from app import app, db
 from app import Restaurant, Pizza, restaurant_pizza
 from flask import request, jsonify
 from datetime import datetime
+
+# Define routes
+
+# Route to get all restaurants
+@app.route('/restaurants', methods=['GET'])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    return jsonify([{'id': r.id, 'name': r.name, 'address': r.address} for r in restaurants])
