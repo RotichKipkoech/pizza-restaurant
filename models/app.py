fc9139027d9c618e0da5df68993c9398978de779
RotichKipkoech <kipkoechrottich@gmail.com>
2023-09-24 21:18:04 +0300

    Defining the Restaurant model

--- models/app.py
+++ models/app.py
@@ -12,3 +12,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///emp.db'
 
 # Creating a SQLAlchemy instance and binding it to the Flask app
 db = SQLAlchemy(app)
+
+# Defining the Restaurant model
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    pizzas = db.relationship('Pizza', secondary='restaurant_pizza', backref='restaurants')
