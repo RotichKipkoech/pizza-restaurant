1edd893a9a38e99e909f04ff649c3e68e14692b5
RotichKipkoech <kipkoechrottich@gmail.com>
2023-09-24 21:19:02 +0300

    Defining the Pizza model

--- models/app.py
+++ models/app.py
@@ -19,3 +19,11 @@ class Restaurant(db.Model):
     name = db.Column(db.String(50), unique=True, nullable=False)
     address = db.Column(db.String(255), nullable=False)
     pizzas = db.relationship('Pizza', secondary='restaurant_pizza', backref='restaurants')
+
+# Defining the Pizza model
+class Pizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    ingredients = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
